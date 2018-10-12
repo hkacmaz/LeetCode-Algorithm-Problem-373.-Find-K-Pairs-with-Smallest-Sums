fed05be2002c52085a8d3bacac5764683a58f699
hkacmaz <halilkacmaz@outlook.com>
2018-10-11 23:16:16 -0700

    libraries are included
    class Solution has been created.

---
+++ p373-4/p373-4/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  p373-4
+//
+//  Created by Halil Kacmaz on 10/11/18.
+//  Copyright © 2018 Halil Kacmaz. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+    }
+};
+
+int main() {
+    
+    Solution so;
+    
+    return 0;
+}
